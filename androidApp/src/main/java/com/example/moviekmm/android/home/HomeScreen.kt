6ac03be81d6586c10a202b35e519e7d21dda1119
aslibayar <aslibyr00@gmail.com>
2025-06-24 16:25:58 +0300

    Add HomeScreen and MovieListItem components for movie display

---
+++ androidApp/src/main/java/com/example/moviekmm/android/home/HomeScreen.kt
@@ -0,0 +1,80 @@
+package com.example.moviekmm.android.home
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
+import androidx.compose.material3.pulltorefresh.PullToRefreshState
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.moviekmm.domain.model.Movie
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    state: PullToRefreshState = rememberPullToRefreshState(),
+    modifier: Modifier = Modifier,
+    uiState: HomeScreenState,
+    loadNextMovies: (Boolean) -> Unit,
+    navigateToDetail: (Movie) -> Unit
+) {
+    PullToRefreshBox(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.background),
+        state = state,
+        isRefreshing = uiState.loading,
+        onRefresh = { loadNextMovies(true) }
+    ) {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            itemsIndexed(uiState.movies, key = { _, movie -> movie.id }) { index, movie ->
+                MovieListItem(
+                    movie = movie,
+                    onClick = navigateToDetail,
+                    modifier = Modifier.fillMaxSize()
+                )
+                if (index == uiState.movies.size - 1 && uiState.loadFinished.not()) {
+                    LaunchedEffect(key1 = Unit, block = { loadNextMovies(false) })
+                }
+            }
+
+            if (uiState.loading && uiState.movies.isNotEmpty()) {
+                item(span = { GridItemSpan(2) }) {
+                    Row(
+                        modifier = modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    ) {
+                        CircularProgressIndicator(
+                            modifier = modifier.align(
+                                Alignment.CenterVertically,
+                            ),
+                            color = MaterialTheme.colorScheme.primary,
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ androidApp/src/main/java/com/example/moviekmm/android/home/MovieListItem.kt
@@ -0,0 +1,65 @@
+package com.example.moviekmm.android.home
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.moviekmm.domain.model.Movie
+
+@Composable
+fun MovieListItem(
+    modifier: Modifier = Modifier,
+    movie: Movie,
+    onClick: (Movie) -> Unit
+) {
+    Card(
+        modifier = modifier
+            .height(250.dp)
+            .clickable { onClick(movie) },
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Column {
+            Box(
+                modifier = modifier.weight(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                AsyncImage(
+                    model = movie.imageUrl,
+                    contentDescription = movie.title,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clip(RoundedCornerShape(bottomStart = 2.dp, bottomEnd = 2.dp))
+                )
+            }
+        }
+        Column(modifier = modifier.padding(10.dp)) {
+            Text(
+                text = movie.title,
+                style = MaterialTheme.typography.bodyLarge,
+                maxLines = 1,
+                modifier = Modifier.padding(bottom = 4.dp)
+            )
+            Text(
+                text = movie.description,
+                style = MaterialTheme.typography.bodyMedium,
+                maxLines = 2,
+                modifier = Modifier.padding(bottom = 4.dp)
+            )
+        }
+    }
+
+}
