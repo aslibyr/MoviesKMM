d998931a527e4928f4a981a0a9a045beb202d4c2
aslibayar <aslibyr00@gmail.com>
2025-07-01 20:46:09 +0300

    Deleted unused files

--- androidApp/src/main/java/com/example/moviekmm/android/common/bottom_bar/BottomNavigationItem.kt
+++
@@ -1,7 +0,0 @@
-package com.example.moviekmm.android.common.bottom_bar
-
-import androidx.compose.ui.graphics.vector.ImageVector
-
-
-data class BottomBarRoute<T : Any>(val name: String, val route: T, val icon: ImageVector)
-
